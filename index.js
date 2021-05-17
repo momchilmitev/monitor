10365bcba566539398fbdd3591fce34f745c8445
momchilmitev <momchilmitev87@gmail.com>
2021-05-17 20:40:10 +0200

    parsing the request paths

--- index.js
+++ index.js
@@ -4,10 +4,22 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
 // Creating the server
 const server = http.createServer((req, res) => {
+  // Get the URL and parse it
+  const parsedURL = url.parse(req.url, true);
+
+  // Get the path
+  const path = parsedURL.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Send response
   res.end('Hi there!');
+
+  // Log the request  path
+  console.log(`Request is received on this path ${trimmedPath}`);
 });
 
 // Starting the server on port 3000
